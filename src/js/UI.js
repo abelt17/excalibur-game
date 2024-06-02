380b008437bd4710671bb55fca3e4616e5f3d737
abelt <113996197+fefeffefeeffefefe@users.noreply.github.com>
2024-06-02 18:49:08 +0200

    update ui.js

--- src/js/UI.js
+++ src/js/UI.js
@@ -1,34 +1,3 @@
-// import { Label, Vector, Font, FontUnit, ScreenElement } from "excalibur";
-// import { ScoreLabel } from "./scorelabel";
-// import { gameState } from "./gamestate";
-
-// export class UI extends ScreenElement {
-
-//     onInitialize(engine) {
-//         this.scoreLabel = new ScoreLabel();
-//         this.addChild(this.scoreLabel);
-//         this.score = 0;
-
-//         if(this.score > 0) {
-//             this.score = 0;
-//         }
-//     }
-    
-//     addPoint(engine) {
-//         this.score++;
-//         this.scoreLabel.text = `Lap: ${this.score}`;
-//         gameState.score = this.score;
-//         if (this.score == 4) {
-//             this.resetScore();
-//         }
-//     }
-
-//     resetScore() {
-//         this.score = 0;
-//         this.scoreLabel.text = `Lap: ${this.score}`;
-//     }
-
-// }
 import { Label, Vector, Font, FontUnit, ScreenElement, Timer, Color } from "excalibur";
 import { ScoreLabel } from "./scorelabel";
 import { gameState } from "./gamestate";
